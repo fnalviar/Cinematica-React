a12f8fea4b0f0206a90020fc056197aa3e68315d
fnalviar <nikka_alviar@outlook.com>
2023-06-20 02:34:12 -0400

    added footer to results page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Movies from "./components/Movies";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import MovieInfo from "./pages/MovieInfo";
-import Movie from "./pages/Movie";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
           <Route path="/movie/:userInput/:imdbID" exact element={<MovieInfo />} />
           <Route path="/movies/:userInput" exact element={<Movies />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
